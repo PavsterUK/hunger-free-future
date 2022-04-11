05f69c645708ea0d6bbb1d939de7269ad6d0fc14
Pavel Naumovich <pavelnaumovic@gmail.com>
2022-04-11 20:51:53 +0100

    heroku deploy

--- src/main/java/co/uk/hungerfree/backend/controller/TownFinderController.java
+++ src/main/java/co/uk/hungerfree/backend/controller/TownFinderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "https://625483720ca58d0009eeb1c9--fantastic-strudel-956d59.netlify.app/")
+@CrossOrigin(origins = "https://625483720ca58d0009eeb1c9--fantastic-strudel-956d59.netlify.app")
 @RestController
 @RequestMapping("/v1/api")
 public class TownFinderController {
